a2d2e9eb9ed3d8dac40740a9cf407a0234894315
Mayra Medina <111921764+Maynh@users.noreply.github.com>
2024-01-19 22:59:20 -0300

    Logica en Programacion JavaScript

---
+++ Logica en Programacion- JavaScript/Desafio/app.js
@@ -0,0 +1,12 @@
+ //Muestra una alerta con el mensaje "¡Bienvenida y bievenido a nuestro sitio web!".
+alert ("¡Bienvenida y bievenido a nuestro sitio web!"); 
+
+
+ //Declara una variable llamada nombre y asígnale el valor "Luna
+ let nombre = "Luna";
+ 
+
+ // Crea una variable llamada edady asígnale el valor 25.
+ let edad = 25;
+
+ //
